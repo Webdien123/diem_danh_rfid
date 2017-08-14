68fd5ca303a33561453e81b9b1866810e2cb43e1
lyvan <vanb1305056@student.ctu.edu.vn>
2017-08-14 13:02:34 +0700

    chuẩn bị giao diện chart

--- Sources/DD_RFID/resources/views/sub_views/chart.blade.php
+++ Sources/DD_RFID/resources/views/sub_views/chart.blade.php
@@ -8,7 +8,7 @@
                     <h3 class="panel-title">Thống kê điểm danh</h3>
                 </div>
                 <div class="panel-body">
-                    Panel content
+                    các biểu đồ
                 </div>
         </div>
 
