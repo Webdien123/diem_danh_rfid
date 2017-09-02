0ab2bdbcd89a22602514ff632ebc82c715ac3b99
Van Tran <vanB1305056@student.ctu.edu.vn>
2017-09-02 19:56:28 +0700

    Cập nhật giao diện đăng nhập.

--- Sources/DD_RFID/resources/views/login.blade.php
+++ Sources/DD_RFID/resources/views/login.blade.php
@@ -10,10 +10,55 @@
     @include('link_views.import')
 </head>
 <body>
-    @include('link_views.header')
-    <h1>Trang đăng nhập</h1>
-    <h2>Nội dung đang cập nhật</h2>
-    <a class="btn btn-default" href="{{ route('admin') }}" role="button">Trang quản trị</a>
-    <a class="btn btn-default" href="{{ route('home') }}" role="button">Trang chủ</a>
+    <div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+                
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Đăng nhập</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form action="#" method="POST" role="form">
+                            @if($errors->has('errorlogin'))
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                    {{$errors->first('errorlogin')}}
+                                </div>
+                            @endif
+                            <div class="form-group">
+                                <label for="">Email</label>
+                                <input type="text" class="form-control" id="email" placeholder="Email" name="email" value="{{old('email')}}">
+                                @if($errors->has('email'))
+                                    <p style="color:red">{{$errors->first('email')}}</p>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                                @if($errors->has('password'))
+                                    <p style="color:red">{{$errors->first('password')}}</p>
+                                @endif
+                            </div>
+                        
+                            
+                            {!! csrf_field() !!}
+                            <button type="submit" class="btn btn-success">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                Đăng nhập
+                            </button>
+    
+                            <a href="{{ route('home') }}" class="btn btn-primary">
+                                <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+                                Về trang chủ
+                            </a>
+                        </form>
+                    </div>
+                </div>
+                
+				
+			</div>
+		</div>
+	</div>
 </body>
 </html>
