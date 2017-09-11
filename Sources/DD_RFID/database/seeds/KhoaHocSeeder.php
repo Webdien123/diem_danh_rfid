a0727f829e7d18cd6293692b516935a7226892d7
Van Tran <vanB1305056@student.ctu.edu.vn>
2017-09-11 19:18:38 +0700

    Tạo file create dữ liệu bảng Khóa Học

---
+++ Sources/DD_RFID/database/seeds/KhoaHocSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KhoaHocSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K35']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K36']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K37']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K38']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K39']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K40']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K41']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K42']);
+        DB::insert('insert into khoahoc (KHOAHOC) values (?)', ['K43']);
+    }
+}
