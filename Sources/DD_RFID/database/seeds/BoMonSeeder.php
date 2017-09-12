98237c738a7547b86c1749f2bd7e217d4dae45ad
Van Tran <vanB1305056@student.ctu.edu.vn>
2017-09-12 19:40:36 +0700

    Cập nhật file sql, seed

--- Sources/DD_RFID/database/seeds/BoMonSeeder.php
+++ Sources/DD_RFID/database/seeds/BoMonSeeder.php
@@ -11,11 +11,11 @@ class BoMonSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Công nghệ thông tin', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Công nghệ phần mềm', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Hệ thống thông tin', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Mạng máy tính và TT', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Khoa học máy tính', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into bomon (TENBOMON) values (?)', ['Tin học ứng dụng', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Công nghệ thông tin', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Công nghệ phần mềm', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Hệ thống thông tin', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Mạng máy tính và TT', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Khoa học máy tính', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into bomon (TENBOMON, TENKHOA) values (?, ?)', ['Tin học ứng dụng', 'Công nghệ thông tin và truyền thông']);
     }
 }
--- Sources/DD_RFID/database/seeds/ChuyenNganhSeeder.php
+++ Sources/DD_RFID/database/seeds/ChuyenNganhSeeder.php
@@ -11,11 +11,11 @@ class ChuyenNganhSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Công nghệ thông tin', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Công nghệ phần mềm', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Hệ thống thông tin', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Mạng máy tính và TT', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Khoa học máy tính', 'Công nghệ thông tin và truyền thông']);
-        DB::insert('insert into chuyennganh (TENCHNGANH) values (?)', ['Tin học ứng dụng', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Công nghệ thông tin', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Công nghệ phần mềm', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Hệ thống thông tin', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Mạng máy tính và TT', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Khoa học máy tính', 'Công nghệ thông tin và truyền thông']);
+        DB::insert('insert into chuyennganh (TENCHNGANH, TENKHOA) values (?, ?)', ['Tin học ứng dụng', 'Công nghệ thông tin và truyền thông']);
     }
 }
