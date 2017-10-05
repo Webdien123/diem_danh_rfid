ec965a6e633d991cfdafd5f95014adca7c4d26ce
Van Tran <vanB1305056@student.ctu.edu.vn>
2017-10-05 17:35:44 +0700

    - Cập nhật lại project backup trước khi sử dụng heroku.

--- Sources/DD_RFID/resources/views/link_views/to_mau_tu_khoa.php
+++ Sources/DD_RFID/resources/views/link_views/to_mau_tu_khoa.php
@@ -1,9 +1,29 @@
 <?php 
+	// Hàm chuyển chữ có dấu về không dấu.
+	// function convert_vi_to_en($str) {
+	// 	$str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+	// 	$str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+	// 	$str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+	// 	$str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+	// 	$str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+	// 	$str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+	// 	$str = preg_replace("/(đ)/", 'd', $str);
+	// 	$str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+	// 	$str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+	// 	$str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+	// 	$str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+	// 	$str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+	// 	$str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+	// 	$str = preg_replace("/(Đ)/", 'D', $str);
+	// 	return $str;
+	// }
+
 	// Hàm thực hiện tô màu phần từ khóa trong chuỗi ban đầu.
 	function ToMau($chuoi_ban_dau, $tukhoa)
-	{		
-		// // Thay chuỗi từ khóa bằng chuỗi có định dạng màu nền.
+	{
+		// $S = preg_replace("/($tukhoa)/i",'<span class="highlight">$1</span>', $chuoi_ban_dau);
+		// Thay chuỗi từ khóa bằng chuỗi có định dạng màu nền.
 		$S = str_ireplace($tukhoa, '<span style="background-color: #ffff33">'.$tukhoa.'</span>', $chuoi_ban_dau );
-		return $S;		
+		return $S;
 	}
 ?>
